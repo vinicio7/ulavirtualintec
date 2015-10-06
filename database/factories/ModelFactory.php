decaf1cc352a4782e197130347f54e6ecd954485
fabriciotorrico <f.abri.1@hotmail.es>
2015-10-06 18:29:30 -0300

    cambio de la bd...aumente algunos campos...y no me acuerdo k mas...pro el cursante ya casi esta...

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -58,11 +58,60 @@ $factory->define(App\Entities\User::class, function(Faker\Generator $faker){
 
 $factory->define(App\Entities\Kardex::class, function(Faker\Generator $faker){
         return [
-            'gestion'    => $faker->name,
-            'activo'     => $faker->boolean(),
-            'user_id'    => $faker->numberBetween($min = 1000, $max = 10000000),
+
             'materia_id' => $faker->numberBetween($min = 1, $max =15),
-            'ua_id'      => $faker->numberBetween($min = 1, $max =3)
+            'user'       => $faker->unique()->numberBetween($min = 1000, $max = 10000000),
+            'gestion'    => $faker->randomElement($array = array ('I/2015','II/2015')),
+            'grupo'      => $faker->numberBetween($min = 1, $max = 5),
+            'activo'     => $faker->boolean(),
+            'ua_id'      => $faker->numberBetween($min = 1, $max =3),
+
+            //Llenar notas
+            //todos los 1 son del parametro "DECIDIR"
+            '11a'       => $faker->numberBetween($min = 1, $max =100),
+            '11b'       => $faker->numberBetween($min = 1, $max =100),
+            '11c'       => $faker->numberBetween($min = 1, $max =100),
+            'prom1'       => $faker->numberBetween($min = 1, $max =100),
+
+            //todos los 2 son del parametro "SABER"
+            '21a'       => $faker->numberBetween($min = 1, $max =100),
+            '21b'       => $faker->numberBetween($min = 1, $max =100),
+            '21c'       => $faker->numberBetween($min = 1, $max =100),
+            '22a'       => $faker->numberBetween($min = 1, $max =100),
+            '22b'       => $faker->numberBetween($min = 1, $max =100),
+            '22c'       => $faker->numberBetween($min = 1, $max =100),
+            'prom2'       => $faker->numberBetween($min = 1, $max =100),
+
+            //todos los 3 son del parametro "HACER"
+            '31a'       => $faker->numberBetween($min = 1, $max =100),
+            '31b'       => $faker->numberBetween($min = 1, $max =100),
+            '31c'       => $faker->numberBetween($min = 1, $max =100),
+            '31d'       => $faker->numberBetween($min = 1, $max =100),
+            '31e'       => $faker->numberBetween($min = 1, $max =100),
+            '31f'       => $faker->numberBetween($min = 1, $max =100),
+            '31g'       => $faker->numberBetween($min = 1, $max =100),
+            'prom3'       => $faker->numberBetween($min = 1, $max =100),
+
+            //todos los 4 son del parametro "SER", evaluan entre cursantes, el facilitador y el Jefe de evaluacion.
+            '41aCursante'       => $faker->numberBetween($min = 1, $max =100),
+            '41bCursante'       => $faker->numberBetween($min = 1, $max =100),
+            '41cCursante'       => $faker->numberBetween($min = 1, $max =100),
+            '41dCursante'       => $faker->numberBetween($min = 1, $max =100),
+            'prom4Cursante'     => $faker->numberBetween($min = 1, $max =100),
+            '41aFacil'          => $faker->numberBetween($min = 1, $max =100),
+            '41bFacil'          => $faker->numberBetween($min = 1, $max =100),
+            '41cFacil'          => $faker->numberBetween($min = 1, $max =100),
+            '41dFacil'          => $faker->numberBetween($min = 1, $max =100),
+            'prom4Facil'        => $faker->numberBetween($min = 1, $max =100),
+
+            '41aJE'             => $faker->numberBetween($min = 1, $max =100),
+            '41bJE'             => $faker->numberBetween($min = 1, $max =100),
+            '41cJE'             => $faker->numberBetween($min = 1, $max =100),
+            '41dJE'             => $faker->numberBetween($min = 1, $max =100),
+            'prom4JE'           => $faker->numberBetween($min = 1, $max =100),
+
+            //El promedio de las tres evaluaciones de 4
+            'prom4'             => $faker->numberBetween($min = 1, $max =100),
         ];
 });
 
