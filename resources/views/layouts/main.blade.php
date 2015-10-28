f87a10d97b01d0d8b3e4df07c02f1bc8e7d512fa
fabriciotorrico <f.abri.1@hotmail.es>
2015-10-28 14:30:41 -0300

    CON FOTOS...28/010/215

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,7 +31,7 @@
             <ul class="nav" id="side-menu">
                 <li class="nav-header">
                     <div class="dropdown profile-element"> <span>
-                            <img alt="image" class="img-circle" src="img/profile_small.jpg" />
+                            <img alt="image" class="img-circle" src="fotosUsuarios/4.jpg" />
                              </span>
                         <!--a data-toggle="dropdown" class="dropdown-toggle" href="#"-->
                         <a>
